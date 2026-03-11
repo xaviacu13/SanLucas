a1f8ce8501a513e4aff820e52767094dff3282ff
xaviacu13 <40414894+xaviacu13@users.noreply.github.com>
2026-03-11 02:33:50 -0300

    add contacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,81 @@
-import logo from "../src/images/logos/logo.png";
+import logo from "../src/assets/images/logos/logo.png";
+import callUp from "../src/assets/documents/convocatoria.pdf";
 import "./App.css";
+import { FaWhatsapp, FaEye, FaDownload } from "react-icons/fa";
+
+const message =
+  "Hola me podria pasar mas informacion sobre el campeonato municipal de San Lucas por favor?";
+
+const contacts = [
+  { name: "Correa Delio", phone: "5491141957971" },
+  { name: "Rodríguez Hernán", phone: "5491150171880" },
+  { name: "Segarra Tomás", phone: "5491136531909" },
+  { name: "Acuña Gerardo", phone: "5491127305021" },
+  { name: "Alvarado Beto", phone: "5491151762134" },
+];
 
 function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={logo} className="logo" alt="Vite logo" />
-        </a>
+        <img src={logo} className="logo" alt="Logo campeonato" />
       </div>
-      <h3>Campeonato de Fútbol intercomunal</h3>
+
+      <h3>Campeonato de Fútbol Intercomunal</h3>
       <h5>San Lucas - 2026</h5>
+
       <p className="read-the-docs">
         Aquí encontrarás toda la información sobre los equipos, jugadores y
         estadísticas del campeonato.
       </p>
-      <p>Xavi Innovation Technology © 2026. All rights reserved.</p>
+
+      <h5>Organiza:</h5>
+
+      <p className="read-the-docs">
+        Monte Pampa - Rodeo Cocha - Tambo K’asa - Sabala - Huayllani Grande
+      </p>
+      <h5>Convocatoria:</h5>
+      <div className="call-up-container">
+        <a
+          href={callUp}
+          // target="_blank"
+          rel="noopener noreferrer"
+          className="call-up-button"
+        >
+          <FaEye />
+          Ver convocatoria
+        </a>
+
+        <a
+          href={callUp}
+          download="Convocatoria 2026.pdf"
+          className="call-up-button download"
+        >
+          <FaDownload />
+          Descargar
+        </a>
+      </div>
+
+      <h5>Contactos:</h5>
+
+      {contacts.map((contact) => (
+        <p key={contact.phone} className="contact">
+          <a
+            href={`https://wa.me/${contact.phone}?text=${encodeURIComponent(
+              message,
+            )}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaWhatsapp className="whatsapp-icon" />
+            {contact.name} - {contact.phone}
+          </a>
+        </p>
+      ))}
+
+      <p className="footer">
+        Xavi Innovation Technology © 2026. All rights reserved.
+      </p>
     </>
   );
 }
