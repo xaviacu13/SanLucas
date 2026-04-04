5d26796ae2fcd40afe91d5ed76614d7815a001c9
xaviacu13 <40414894+xaviacu13@users.noreply.github.com>
2026-04-03 21:01:00 -0300

    setting building error

--- src/constants/teamCategories/infantil.tsx
+++ src/constants/teamCategories/infantil.tsx
@@ -4,6 +4,7 @@ import type { ITeamCategoryItem } from "../../types/types";
 export const infantil: ITeamCategoryItem[] = [
   {
     id: 1,
+    series: "A",
     name: "Tira Hoyo",
     url: "",
     logo: getLogo("Tira Hoyo"),
@@ -14,6 +15,7 @@ export const infantil: ITeamCategoryItem[] = [
   },
   {
     id: 2,
+    series: "A",
     name: "Puca Loma",
     url: '',
     // url: "https://forms.gle/Pa2yze8SBKPHRWdd9",
@@ -24,113 +26,5 @@ export const infantil: ITeamCategoryItem[] = [
       { id: 3, name: "Marizol Moscoso", contact: "", category: "Damas" },
     ],
   },
-  {
-    id: 3,
-    name: "Miska Mayu",
-    url: '',
-    // url: "https://forms.gle/cSRUrK9pG1EXhejH8",
-    logo: getLogo("Miska Mayu"),
-    delegates: [
-      { id: 1, name: "Moises Moscoso", contact: "", category: "Todos" },
-    ],
-  },
-  {
-    id: 4,
-    name: "Monte Pampa",
-    url: "",
-    logo: getLogo("Monte Pampa"),
-    delegates: [
-      { id: 1, name: "Feliciano Alvarado", contact: "", category: "Todos" },
-      { id: 2, name: "Jorge Luis Alvarado", contact: "", category: "Todos" },
-    ],
-  },
-  {
-    id: 5,
-    name: "Sivinga Mayo",
-    url: "",
-    logo: getLogo("Sivinga Mayu"),
-    delegates: [
-      { id: 1, name: "Wilber Apaza", contact: "", category: "Todos" },
-      { id: 2, name: "Enzo Quira", contact: "", category: "Juvenil"}
-    ],
-  },
-  {
-    id: 6,
-    name: "Punquina",
-    url: "",
-    logo: getLogo("Punquina"),
-    delegates: [
-      {
-        id: 1,
-        name: "Gustavo Ninaja Quispe",
-        contact: "",
-        category: "Juvenil",
-      },
-      {
-        id: 2,
-        name: "Vicmar Paco Mollo",
-        contact: "",
-        category: "Juvenil",
-      },
-      { id: 2, name: "Reina Mollo", contact: "", category: "Damas" },
-    ],
-  },
-  {
-    id: 7,
-    name: "Rio Chico",
-    url: '',
-    // url: "https://forms.gle/u3QfZPs82JJGBDdE8",
-    logo: getLogo("Rio Chico"),
-    delegates: [
-      { id: 1, name: "Iver Condori F.", contact: "", category: "Todos" },
-    ],
-  },
-  {
-    id: 8,
-    name: "Padcoyo",
-    url: "",
-    logo: getLogo("Padcoyo"),
-    delegates: [
-      { id: 1, name: "Wilfredo Mamani", contact: "", category: "Todos" },
-    ],
-  },
-  {
-    id: 9,
-    name: "Kumuni",
-    url: "",
-    logo: getLogo("Kumuni"),
-    delegates: [
-      { id: 1, name: "Modesto Huallpa", contact: "", category: "Varones" },
-      { id: 2, name: "Fredy Condori", contact: "", category: "Varones" },
-      { id: 3, name: "Eva Colque", contact: "", category: "Damas" },
-    ],
-  },
-  {
-    id: 10,
-    name: "Churqui Pampa",
-    url: "",
-    logo: getLogo("Churqui Pampa"),
-    delegates: [
-      { id: 1, name: "Sebastian Cruz", contact: "", category: "Todos" },
-    ],
-  },
-  // {
-  //   id: 11,
-  //   name: "Andajaba",
-  //   url: "",
-  //   logo: getLogo("Andajaba"),
-  //   delegates: [
-  //     { id: 1, name: "Ibania Condori M.", contact: "", category: "Todos" },
-  //   ],
-  // },
-    {
-    id: 11,
-    name: "Miska Mayu 'B'",
-    url: '',
-    // url: "https://forms.gle/cSRUrK9pG1EXhejH8",
-    logo: getLogo("Miska Mayu 'B'"),
-    delegates: [
-      { id: 2, name: "Ismael Moscoso", contact: "", category: "Juvenil" },
-    ],
-  },
+ 
 ];
