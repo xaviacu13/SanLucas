3d85e49a92bb5f26c5ec5326a41043515c6c2d33
xaviacu13 <40414894+xaviacu13@users.noreply.github.com>
2026-05-07 11:19:12 -0300

    remove limited goals

--- src/pages/TopScorerTable/TopScorerTable.tsx
+++ src/pages/TopScorerTable/TopScorerTable.tsx
@@ -57,7 +57,7 @@ const TopScorerTable: React.FC = () => {
       const data = await getTopScorersWithPlayers(
         matches,
         selectedCategory,
-        3,
+       // 3, // Limitar a los 3 mejores goleadores por categoría
       );
 
       setScorers(data);
