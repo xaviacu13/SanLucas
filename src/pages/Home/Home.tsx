9ca9e1c4589aa33e583a40e8631fe0d8bfbc524e
xaviacu13 <40414894+xaviacu13@users.noreply.github.com>
2026-06-09 09:01:42 -0300

    refresh update date

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -128,7 +128,7 @@ const Home: React.FC = () => {
           <UpdateInfo>
             <LiveDot />
             <span>
-              Última actualización: <strong>03 Junio 2026 • 23:25 </strong> hs
+              Última actualización: <strong>09 Junio 2026 • 09:00 </strong> hs
             </span>
           </UpdateInfo>
           {/* <p>
